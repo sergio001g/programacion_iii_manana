c2286b12b0a3507671df98f4cb83a9095f50adaf
sergiolbqr <sergio.gomesc13@outlook.es>
2025-10-16 10:04:19 -0500

    Subir contenido a semana3: modulo2/javascript (métodos, variables, operadores); correcciones en 02_variables HTML/JS

---
+++ modulo2/javascript/02_variables.js
@@ -0,0 +1,42 @@
+console.log('Variables');
+
+const saludos = "hola utilozando varibles ";
+console.log("saludos:", saludos);
+console.log("tipos de variables saludos:", typeof saludos);
+
+const numero = 256;
+console.log("variable de numero:", numero);
+console.log("tipos de variables ", typeof numero);
+
+const boo = true;
+console.log("variable de booleana:", boo);
+console.log("tipos de variables ", typeof boo);
+
+const indef = undefined;
+console.log("variable indefinida:", indef);
+console.log("tipos de variables ", typeof indef);
+
+const nula = null;
+console.log("variable nula:", nula);
+console.log("tipos de variables ", typeof nula);
+
+const bigentero = 3984928238n; 
+console.log("variable bigint:", bigentero);
+console.log("tipos de variables ", typeof bigentero);
+
+
+persona = {
+    nombre: "Sergio",
+    edad: 19,
+};
+
+
+console.log("variable objeto:", persona);
+console.log("tipos de variables ", typeof persona);
+
+
+numeros = [1, 2, 3, 4, 5];
+console.log("variable array:", numeros);
+console.log("tipos de variables ", typeof numeros);
+
+
